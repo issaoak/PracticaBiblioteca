f60e2ad4708abc9b8a22b608e341c358833660ac
issaoak <85519921+issaoak@users.noreply.github.com>
2024-10-29 19:50:19 -0600

    Añadimos los controladores para vista y validacion

--- app/Http/Controllers/LibrosController.php
+++ app/Http/Controllers/LibrosController.php
@@ -1,10 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LibrosController extends Controller
+class LibroController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('principal');
+    }
+    public function create()
+    {
+        return view('registro');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'isbn' => 'required|numeric|digits:13',
+            'titulo' => 'required|string|max:150',
+            'autor' => 'required|string|max:100',
+            'paginas' => 'required|integer|min:1',
+            'año' => 'required|integer|min:1000|max:' . date('Y'),
+            'editorial' => 'required|string|max:100',
+            'email' => 'required|email'
+        ]);
+        return redirect()->route('registro')->with('success', 'Todo correcto: Libro "' . $request->titulo . '" guardado');
+    }
 }
