e0598e7d4a1736ae4429074347abfcbdd3225ecd
issaoak <85519921+issaoak@users.noreply.github.com>
2024-10-29 20:05:11 -0600

    Se añaden la rutas para las vistas

--- app/Http/Controllers/LibrosController.php
+++ app/Http/Controllers/LibrosController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class LibroController extends Controller
+class LibrosController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibrosController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LibrosController::class, 'index'])->name('principal');
+Route::get('/registro', [LibrosController::class, 'create'])->name('registro');
+Route::post('/registro', [LibrosController::class, 'store'])->name('libro.store');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
